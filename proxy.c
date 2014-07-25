4fc4e2823db15548efd4e894bdbd5dbc9e0f8c5c
Jeff Brandon <brandj@cmu.edu>
2014-07-25 17:38:10 -0400

    compiles but does nothing

--- proxy.c
+++ proxy.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-struct http_request {
+struct req_t {
     char* header;
     char* domain;
     char* path;
 };
-typedef struct http_request req_t;
+typedef struct req_t req_t;
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct hostent hostent;
 
 /* You won't lose style points for including these long lines in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -19,26 +24,36 @@ static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 
 int main(int argc, char** argv)
 {
-    int listenport;
-    req_t request;
-    printf("%s%s%s", user_agent_hdr, accept_hdr, accept_encoding_hdr);
+    int listenport, listenfd, connfd;
+    unsigned clientlen;
+    sockaddr_in clientaddr;
+    hostent* clientinfo;
+    char* haddrp;
 
-    //psuedocode for getting started
-    
+    user_agent_hdr = user_agent_hdr;
+    accept_hdr = accept_hdr;
+    accept_encoding_hdr = accept_encoding_hdr;
+
     if(argc < 2){
         printf("usage: %s <port number to bind and listen>\n", argv[0]);
         exit(1);
     }
-    listenport = get_port(argv);
+    listenport = atoi(argv[1]);
+    listenfd = open_listenfd(listenport);
+    if(listenfd == -1){
+        fprintf(stderr,"error opening listening socket\n");
+        exit(1);
+    }
     
-    //create socket
-    //bind
-    //listen
     while(1){
-        //accept
-        request = process_request();
-        response = forward_request(request);
-        forward_response(response);
+        clientlen = sizeof(clientaddr);
+        connfd = accept(listenfd, (SA *) &clientaddr, &clientlen); 
+        clientinfo = gethostbyaddr((const char*)&clientaddr.sin_addr.s_addr,
+                                   sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        haddrp = inet_ntoa(clientaddr.sin_addr);
+        //request = process_request();
+        //response = forward_request(request);
+        //forward_response(response);
     }
 
     return 0;
