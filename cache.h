9f57fc118a08d929f55f6a8f2863748e18b59c50
Cpruce <Cpruce@users.noreply.github.com>
2014-07-30 15:42:18 -0400

    cache correctness needs fixing

--- cache.h
+++ cache.h
@@ -7,17 +7,22 @@
 #define MAX_OBJECT_SIZE 102400
 
 typedef struct {
-	char * name;
-	char * buf;
-	int obj_size;
-	int age;
+    unsigned obj_size;
+    int age;
+    char * name;
+    char * buf;
+    struct cache_obj* next;
 } cache_obj;
 
+
 cache_obj* in_cache(char* name, int num_entries, cache_obj* cache);
-cache_obj* cache_init(cache_obj* cache);
-void cache_deinit(cache_obj* cache);
+cache_obj* cache_init(cache_obj* cache, char * name, char buf[]);
+cache_obj* add_obj(cache_obj* cache, int num_entries, char* name, char buf[]);
+cache_obj* free_spot(cache_obj* entry);
+void cache_deinit(cache_obj* cache, int num_entries);
 int size(cache_obj* cache, int num_entries);
-int cache_write(char* name, char buf[], int buf_bytes, int num_entries, cache_obj* cache);
-cache_obj* cache_evict(cache_obj* cache, int num_entries);
+cache_obj* cache_write(char* name, char buf[], int num_entries, cache_obj* cache);
+cache_obj* cache_evict(cache_obj* cache, int num_entries, size_t alloc_size);
 
 #endif /* __CACHE_H__ */
+
